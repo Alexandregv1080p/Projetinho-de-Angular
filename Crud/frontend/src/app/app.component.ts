1a644bd4db5878f9754cdbb40364581705d9fb85
Alexandregv1080p <alexandregv1999@gmail.com>
2022-10-26 14:05:55 -0300

    terminando a side nav

--- Crud/frontend/src/app/app.component.ts
+++ Crud/frontend/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `app-component.html`,
-  styles: []
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
   title = 'frontend';
