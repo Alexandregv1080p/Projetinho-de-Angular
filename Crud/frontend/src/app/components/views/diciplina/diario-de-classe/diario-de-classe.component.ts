ddbbdf6f04bb4f24ca1092bb31121dd82deecc9a
Alexandre Gomes Vieira <84604919+Alexandregv1080p@users.noreply.github.com>
2022-11-04 15:00:30 -0300

    commit

---
+++ Crud/frontend/src/app/components/views/diciplina/diario-de-classe/diario-de-classe.component.ts
@@ -0,0 +1,29 @@
+import {SelectionModel} from '@angular/cdk/collections';
+import {Component} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+];
+
+@Component({
+  selector: 'app-diario-de-classe',
+  templateUrl: './diario-de-classe.component.html',
+  styleUrls: ['./diario-de-classe.component.css']
+})
+export class DiarioDeClasseComponent{
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+   
+
+
+}
